7df32cc3714b257d33f82a98cdd7281a5cb5d643
Lauren Okada <lauren.okada@alleninstitute.org>
2021-08-19 16:15:17 +0000

    improve prop_summary funciton to handle multiple groupby or summarize columns

--- categorical_comparison.R
+++ categorical_comparison.R
@@ -4,17 +4,16 @@
 #' Creates a dataframe with calculted percentages of categorical variable levels within
 #' another categorical variable
 #'  
-#' @param 
-#' 
+#' @param df A data frame
+#' @param groupby_col Character vector. One or more column names from df.
+#' @param sumcol Character vector. One or more column names from df.
 #'
-#'
-#'
-prop_summary <- function(df, bycol, sumcol){
+prop_summary <- function(df, groupby_col, sumcol){
   df <- df %>%
-    dplyr::group_by(!!as.name(bycol), !!as.name(sumcol)) %>%
+    dplyr::group_by(across(all_of(c(bycol, sumcol)))) %>%
     dplyr::summarize(N = n(), .groups = "drop") %>%
-    dplyr::group_by(!!as.name(bycol)) %>%
-    dplyr::mutate(N_all = sum(N), 
+    dplyr::group_by(across(all_of(sumcol))) %>%
+    dplyr::mutate(N_all_group = sum(N), 
                   Pct = N/sum(N)*100) %>%
     dplyr::ungroup()
   df
